9a8d5a41de331602fec73d1db7a35d53a86a8582
Jaroslaw <roszyk.jarek@gmail.com>
2023-08-18 22:03:20 +0200

    Refactor

--- main.cpp
+++ main.cpp
@@ -22,6 +22,10 @@ std::vector<std::string> readfile(const std::string& file)
 {
     std::vector<std::string> result{};
     std::ifstream inf(file);
+    if (!inf.is_open())
+    {
+        throw std::runtime_error("Error opening file: " + file);
+    }
 
     std::string line{};
     while (std::getline(inf, line))
@@ -32,12 +36,18 @@ std::vector<std::string> readfile(const std::string& file)
             result.push_back(cleanedLine);
         }
     }
+    inf.close();
     return result;
 }
 
 void saveToNewFile(const std::vector<std::string>& processedLines)
 {
     std::ofstream outputFile("output.txt");
+    if (!outputFile.is_open())
+    {
+        throw std::runtime_error("Error creating output file.");
+    }
+
     for (const std::string& line : processedLines)
     {
         outputFile << line << std::endl;
@@ -53,23 +63,42 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    std::string folderPath = argv[1];
-    std::vector<std::string> processedLines{};
-
-    for (const auto& entry : fs::directory_iterator(folderPath))
+    try
     {
-        if (entry.is_regular_file())
+        std::string folderPath = argv[1];
+
+        if (not fs::exists(folderPath) || not fs::is_directory(folderPath))
         {
-            std::vector<std::string> lines = readfile(entry.path());
-            processedLines.insert(processedLines.end(), lines.begin(), lines.end());
+            throw std::runtime_error("Invalid folder path: " + folderPath);
         }
+
+        std::vector<std::string> processedLines{};
+        for (const auto& entry : fs::directory_iterator(folderPath))
+        {
+            if (entry.is_regular_file())
+            {
+                std::vector<std::string> lines = readfile(entry.path());
+                processedLines.insert(processedLines.end(), lines.begin(), lines.end());
+            }
+        }
+
+        if (processedLines.empty())
+        {
+            std::cerr << "No valid lines found in the specified files." << std::endl;
+            return 1;
+        }
+
+        std::sort(processedLines.begin(), processedLines.end());
+
+        auto print = [](const auto& container) { for (const auto& el : container) { std::cout << el << std::endl; } };
+        print(processedLines);
+        saveToNewFile(processedLines);
     }
-
-    std::sort(processedLines.begin(), processedLines.end());
-
-    auto print = [](auto& container) { for (auto& el : container) { std::cout << el << std::endl; } };
-    print(processedLines);
-    saveToNewFile(processedLines);
+    catch (const std::exception& e)
+    {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
